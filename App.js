86118f62e3b5e390831ffe53a68599c077a174ae
Hydro-BoB <54343070+Hydro-BoB@users.noreply.github.com>
2023-03-05 23:32:56 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
-import React, { useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import TodoList from './TodoList';
-import uuidv4 from 'uuid/v4';
+//import uuidv4 from 'uuid/v4';
+
+// function uid() {
+//   for(int i)
+// }
+const LOCAL_STORAGE_KEY = 'todoApp. todos'
+
 
 function App() {
   const [todos, setTodos] = useState([]) //{id:1 , name:'Todo 1', complete:false}
   
   const todoNameRef= useRef()
 
+  //storing everything  , parse is to change data type
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem
+    (LOCAL_STORAGE_KEY) )
+    if (storedTodos) setTodos(storedTodos)
+  },[])
+
+    useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos) )
+    }, [todos])
+
+
  function handleAddTodo(e) {
    const name = todoNameRef.current.value
    if(name==='') return
    setTodos(prevTodos => {
-    return [...prevTodos , { id: uuidv4(), name: name , complete:false }]
+    return [...prevTodos , { id: Math.random()*100 , name: name , complete:false } ]
+    //                          uuidv4()
   })
    //console.log(name)        
    todoNameRef.current.value=null //show nothing in input rather than backspacing all the letters (better quality of life change)
